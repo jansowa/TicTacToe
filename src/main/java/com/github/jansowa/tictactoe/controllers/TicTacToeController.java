2f06a6835d3cb65d8e99646fb01eb8dae613c3cb
Jan Sowa <jan.piotr.sowa@gmail.com>
2018-06-17 13:40:44 +0200

    Add functions in TicTacToeController: getBoard, restartBoard, singleMove

--- src/main/java/com/github/jansowa/tictactoe/controllers/TicTacToeController.java
+++ src/main/java/com/github/jansowa/tictactoe/controllers/TicTacToeController.java
@@ -1,8 +1,39 @@
 package com.github.jansowa.tictactoe.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class TicTacToeController {
+import com.github.jansowa.tictactoe.domain.TicTacToeBoard;
+import com.github.jansowa.tictactoe.game.TicTacToeMechanics;
 
+@RestController
+@Component
+public class TicTacToeController {
+	@Autowired
+	TicTacToeBoard board;
+	@Autowired
+	TicTacToeMechanics mechanics;
+	
+	@GetMapping("/getBoard")
+	TicTacToeBoard getBoard(){
+		return this.board;
+	}
+	
+	@GetMapping("/restartBoard")
+	TicTacToeBoard restartBoard(){
+		mechanics.restartBoard();
+		return this.board;
+	}
+	
+	@PostMapping("/singleMove")
+	TicTacToeBoard singleMove(
+		@RequestParam String field
+		){
+		mechanics.singleMove(field);
+		return this.board;
+	}
 }
