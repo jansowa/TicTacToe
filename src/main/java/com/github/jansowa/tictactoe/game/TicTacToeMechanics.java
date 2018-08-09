872ccf9a45a80c4ec69fec9ee79942b762e79e70
Jan Sowa <jan.piotr.sowa@gmail.com>
2018-08-09 19:28:31 +0200

    Fix whole mechanics class for new data types -change String for Move -change one-dimenasional arrays to two-dimensional arrays

--- src/main/java/com/github/jansowa/tictactoe/game/TicTacToeMechanics.java
+++ src/main/java/com/github/jansowa/tictactoe/game/TicTacToeMechanics.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.github.jansowa.boardGame.mechanics.BoardMechanics;
+import com.github.jansowa.boardGame.mechanics.Move;
 import com.github.jansowa.boardGame.domain.GameBoard;
 import com.github.jansowa.tictactoe.domain.TicTacToeBoard;
 
@@ -27,35 +28,35 @@ public class TicTacToeMechanics extends BoardMechanics {
 	@Override
 	public int isGameOver() {
 		//-1 not over, 0 player O wins, 1 player X wins, 2 draw game
-		int[] fields = this.getBoard().getFields();
+		int[][] fields = this.getBoard().getFields();
 		//checks first horizontal line, first vertical line and first diagonal line
-		if((fields[0]!=-1) &&
-			((fields[0]==fields[1] && fields[1]==fields[2]) ||
-			(fields[0]==fields[4] && fields[4]==fields[8]) ||
-			(fields[0]==fields[3] && fields[3]==fields[6]))){
-				return fields[0];
+		if((fields[0][0]!=-1) &&
+			((fields[0][0]==fields[0][1] && fields[0][1]==fields[0][2]) ||
+			(fields[0][0]==fields[1][1] && fields[1][1]==fields[2][2]) ||
+			(fields[0][0]==fields[1][0] && fields[1][0]==fields[2][0]))){
+				return fields[0][0];
 		}
 		
 		//checks second vertical line
-		if(fields[1]!=-1 && fields[1]==fields[4] && fields[4]==fields[7]){
-			return fields[1];
+		if(fields[0][1]!=-1 && fields[0][1]==fields[1][1] && fields[1][1]==fields[2][1]){
+			return fields[0][1];
 		}
 		
 		//checks third vertical line and one diagonal line
-		if((fields[2]!=-1) &&
-			((fields[2]==fields[5] && fields[5]==fields[8])||
-			(fields[2]==fields[4] && fields[4]==fields[6]))){
-				return fields[2];
+		if((fields[0][2]!=-1) &&
+			((fields[0][2]==fields[1][2] && fields[1][2]==fields[2][2])||
+			(fields[0][2]==fields[1][1] && fields[1][1]==fields[2][0]))){
+				return fields[0][2];
 		}
 		
 		//checks second horizontal line
-		if(fields[3]!=-1 && fields[3]==fields[4] && fields[4]==fields[5]){
-			return fields[3];
+		if(fields[1][0]!=-1 && fields[1][0]==fields[1][1] && fields[1][1]==fields[1][2]){
+			return fields[1][0];
 		}
 		
 		//checks third horizontal line
-		if(fields[6]!=-1 && fields[6]==fields[7] && fields[7]==fields[8]){
-			return fields[6];
+		if(fields[2][0]!=-1 && fields[2][0]==fields[2][1] && fields[2][1]==fields[2][2]){
+			return fields[2][0];
 		}
 		if(this.emptyFields()==0){
 			return 2;
@@ -65,10 +66,13 @@ public class TicTacToeMechanics extends BoardMechanics {
 
 	@Override
 	public void restartBoard() {
-		for(int i=0; i<9; i++){
-			this.getBoard().getFields()[i]=-1;
-			this.getBoard().setPlayer(0);
-		}
+		int[][] empty = {
+				{-1, -1, -1},
+				{-1, -1, -1},
+				{-1, -1, -1}
+		};
+		this.getBoard().setFields(empty);
+		this.getBoard().setPlayer(0);
 	}
 
 	@Override
@@ -78,37 +82,45 @@ public class TicTacToeMechanics extends BoardMechanics {
 	
 	public static int emptyFields(GameBoard board){
 		int counter=0;
-		int[] fields = board.getFields();
-		for(int i=0; i<9; i++){
-			if(fields[i]==-1){
-				counter++;
+		int[][] fields = board.getFields();
+		for(int i=0; i<3; i++){
+			for(int j=0; j<3; j++){
+				if(fields[i][j]==-1){
+					counter++;
+				}
 			}
 		}
 		return counter;
 	}
 	
 	@Override
-	public void changeBoard(String field){
-		int fields[] = this.getBoard().getFields();
+	public void changeBoard(Move move){
+		int fields[][] = this.getBoard().getFields();
 		int currentPlayer = this.getBoard().getPlayer();
-		fields[strFieldToIntField(field)]=currentPlayer;
+		//TODO TO FIX!!
+		int y = move.getCoordinates().getY();
+		int x = move.getCoordinates().getX();
+		fields[y][x]=currentPlayer;
 		this.getBoard().setPlayer((currentPlayer+1)%2);
 	};
 
 	@Override
-	public boolean isMovePossible(String field) {
-		int[] fields = this.getBoard().getFields();
-		if(fields[strFieldToIntField(field)]!=-1){
+	public boolean isMovePossible(Move move) {
+		int[][] fields = this.getBoard().getFields();
+		int y = move.getCoordinates().getY();
+		int x = move.getCoordinates().getX();
+		if(fields[y][x]!=-1){
 			return false;
 		}
 		return true;
 	}
-	
-	public static int strFieldToIntField(String field){
-		return (((int) field.charAt(0))-65)*3 + ((int) field.charAt(1))-49;
+
+	public static Move stringFieldToMove(String field){
+		return new Move((int) field.charAt(0)-65, (int) field.charAt(1)-49);
 	}
 	
-	public static String intFieldToStringField(int field){
-		return ""+(char) (field/3+65)+(char) (field%3 + 49);
+	public static String moveToStringField(Move move){
+		return ""+(char) (move.getCoordinates().getY()+65) +
+				Integer.toString(move.getCoordinates().getX()+1);
 	}
 }
